7610d094c6d48b6f50c18ccc4a855e8ddca8470d
Adriano Costa <spike.summer@hotmail.com>
2023-10-31 22:43:46 -0300

    first commit

---
+++ api_core/data.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    [
+        'name' => 'joao',
+        'email' => 'joao@gmail.com',
+    ],
+    [
+        'name' => 'miguel',
+        'email' => 'miguel@hotmail.com',
+    ],
+    [
+        'name' => 'maria',
+        'email' => 'maria@gmail.com',
+    ],
+    [
+        'name' => 'joana',
+        'email' => 'joana@outlook.com',
+    ],
+    [
+        'name' => 'angelo',
+        'email' => 'angelo@gmail.com',
+    ],
+    [
+        'name' => 'felipe',
+        'email' => 'felipe@hotmail.com',
+    ]
+];
---
+++ public/v1/get_person_data/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/../../../api_core/config.php';
+require_once __DIR__ . '/../../../api_core/response.php';
+
+$data = require_once __DIR__ . '/../../../api_core/data.php';
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+}else{
+    echo Response::json(400, 'error', 'Missing id parameter');
+    exit;
+}
+
+if($id < 0 || $id > count($data) - 1){
+    echo Response::json(400, 'error', 'Person not found');
+}
+
+echo Response::json(200, 'success', $data[$id]);
